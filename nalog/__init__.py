93f73456949ec3636b9e17d212792228b2e623c3
Daniil <dadyarri@gmail.com>
2020-05-21 18:39:29 +0300

    Add library to working with FTS API.

---
+++ nalog/__init__.py
@@ -0,0 +1,90 @@
+import requests
+
+
+class Nalog:
+    """Реализация подключения к API Налоговой службы
+
+    Args:
+        phone: Номер телефона для авторизации в сервисах ФНС
+        password: Пароль для авторизации в сервисах ФНС
+    """
+
+    def __init__(self, phone: str, password: str):
+        """
+        Инициализация API.
+        """
+        self.phone = phone
+        self.password = password
+
+    def exist_receipt(self, fn, n, i, fp, t, s):
+        """
+        Проверка существования чека
+
+        Args:
+            fn: Фискальный номер. 16-значное число
+            i: Фискальный документ: Число до 10 знаков
+            fp: Фискальный признак документа: Число до знаков
+            n: Вид кассового чека (1 - приход, 2 - возрат прихода)
+            t: Дата совершения покупки
+            s: Сумма чека в копейках
+        """
+        exist_url = (
+            f"https://proverkacheka.nalog.ru:9999/v1/ofds/*/inns/*/fss/"
+            f"{fn}/operations/{n}/tickets/{i}"
+        )
+        receipt_data = {
+            "fiscalSign": fp,
+            "date": t,
+            "sum": s,
+        }
+        query = requests.get(exist_url, receipt_data, auth=(self.phone, self.password))
+
+        if query.status_code == 204:
+            return True
+        if query.status_code == 406:
+            return False
+
+    def get_full_data_of_receipt(self, fn, i, fp, *_):
+        """Получение подробной информации о чеке
+        Arguments:
+            fn: Фискальный номер. 16-значное число
+            i: Фискальный документ: Число до 10 знаков
+            fp: Фискальный признак документа: Число до знаков
+        """
+        full_url = (
+            f"https://proverkacheka.nalog.ru:9999/v1/inns/*/kkts/*/fss"
+            f"/{fn}/tickets/{i}?fiscalSign={fp}&sendToEmail=no"
+        )
+
+        query = requests.get(
+            full_url,
+            headers={"device-id": "", "device-os": ""},
+            auth=(self.phone, self.password),
+        )
+
+        if query.status_code == 406:
+            print("Чек не найден")
+            receipt = Receipt()
+        elif query.status_code == 202:
+            print("Существование чека не было проверено")
+            receipt = Receipt()
+        elif query.status_code == 200:
+            receipt = Receipt()
+            for item in query.json()["document"]["receipt"]["items"]:
+                receipt.items.append(Purchase(**item))
+        else:
+            receipt = Receipt()
+        return receipt
+
+
+class Receipt:
+    items = []
+
+
+class Purchase:
+    def __init__(self, name: str, price: int, quantity: float, sum: int):
+        self.name: str = name
+        self.price: float = price / 100
+        self.quantity: float = quantity
+        self.sum: float = sum / 100
+        self.category: str = ""
