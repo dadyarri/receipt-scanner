6cd04a08ae8ce7d719a597d3ed2d709d5a84dec4
Daniil <dadyarri@gmail.com>
2020-07-28 18:10:09 +0300

    Add output of line number in logger formatter.

--- logger.py
+++ logger.py
@@ -9,15 +9,16 @@ class BaseFormatter(logging.Formatter):
         msg = record.msg
         ln = record.levelname
         mdl = record.module
+        lno = record.lineno
         if record.exc_info:
             trs = traceback.format_exception(*record.exc_info)
         else:
             trs = None
-        return {"msg": msg, "ln": ln, "mdl": mdl, "trs": trs}
+        return {"msg": msg, "ln": ln, "mdl": mdl, "trs": trs, "lno": lno}
 
     def format(self, record: logging.LogRecord) -> str:
         data = self.construct_format(record)
-        fmt = f"[{data['ln']}] ({data['mdl']}): {data['msg']}"
+        fmt = f"[{data['ln']}] ({data['mdl']}:{data['lno']}): {data['msg']}"
         if record.exc_info is not None:
             fmt += f"\n{''.join(data['trs'])}"
         return fmt
