e45cae733798c05e2f76785562ca73360af68c44
Daniil <dadyarri@gmail.com>
2020-07-28 19:35:25 +0300

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="receipt-scanner",
+    version="3.0",
+    packages=find_packages(),
+    package_data={"": ["*.yml"]},
+    install_package_data=True,
+    install_requires=[
+        "requests",
+        "pillow",
+        "pyzbar",
+        "pyyaml",
+        "matplotlib",
+        "pandas",
+        "tabulate",
+        "click",
+    ],
+    python_requires=">=3.8",
+    entry_points="""
+        [console_scripts]
+        rc=main:main
+    """,
+    author="dadyarri",
+    author_email="dadyarri@gmail.com",
+    description="Receipt scanner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dadyarri/receipt-scanner",
+)
